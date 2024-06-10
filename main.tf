39569e787ed62858a64b338746a205c3bd815607
tekore <tekore@tutamail.com>
2024-06-10 16:42:43 +0200

    small update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
   host = "192.168.1.250:16443"
-  client_certificate     = "${file("./../client.crt")}"
-  client_key             = "${file("./../client.key")}"
-  cluster_ca_certificate = "${file("./../ca.crt")}"
+  client_certificate     = "${file("./../mk8s-certs/client.crt")}"
+  client_key             = "${file("./../mk8s-certs/client.key")}"
+  cluster_ca_certificate = "${file("./../mk8s-certs/ca.crt")}"
 }
